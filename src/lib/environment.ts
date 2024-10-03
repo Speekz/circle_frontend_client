13503b7debe11ebc06bdd1db431f407ddc3c16c1
Johan Bejar <johanbejarc@hotmail.com>
2024-10-03 11:18:00 -0500

    feat(payments-store): setup a max count of payments to be stored

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -1,2 +1,6 @@
 export const NEXT_PUBLIC_CIRCLE_API =
   process.env.NEXT_PUBLIC_CIRCLE_API || "http://localhost:8080";
+export const NEXT_PUBLIC_MAX_PAYMENTS_COUNT =
+  parseInt(process.env.NEXT_PUBLIC_MAX_PAYMENTS_COUNT || "") === 0
+    ? 1000
+    : parseInt(process.env.NEXT_PUBLIC_MAX_PAYMENTS_COUNT || "");
--- src/store/usePaymentsStore.ts
+++ src/store/usePaymentsStore.ts
@@ -1,3 +1,4 @@
+import { NEXT_PUBLIC_MAX_PAYMENTS_COUNT } from "@/lib/environment";
 import { IPayment } from "@/lib/types";
 import { create } from "zustand";
 
@@ -10,9 +11,19 @@ interface IUsePaymentsStore {
 export const usePaymentsStore = create<IUsePaymentsStore>()((set, get) => ({
   payments: [],
   addPayment: (newPayment: IPayment) =>
-    set(({ payments }) => ({
-      payments: [newPayment, ...payments],
-    })),
+    set(({ payments }) => {
+      const modifiedPayments = payments;
+      if (payments.length >= NEXT_PUBLIC_MAX_PAYMENTS_COUNT) {
+        modifiedPayments.pop();
+        modifiedPayments.unshift(newPayment);
+      } else {
+        modifiedPayments.unshift(newPayment);
+      }
+
+      return {
+        payments: modifiedPayments,
+      };
+    }),
   getPayments: (count: number) => {
     if (get().payments.length > count) {
       return get().payments.slice(0, count);
