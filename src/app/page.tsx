865213a2ee69dd5498238995d96008c4cf5a1892
Johan Bejar <johanbejarc@hotmail.com>
2024-10-03 13:25:45 -0500

    feat(transactiontable): added live and pause handling on client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,49 @@
 "use client";
+import { usePaymentsStore } from "@/store/usePaymentsStore";
+import { Button } from "@headlessui/react";
 import TransactionTable from "@/components/organisms/TransactionTable/TransactionTable";
+import classnames from "classnames";
 
 export default function Home() {
+  const { livePayments, toggleLivePayments } = usePaymentsStore(
+    (state) => state
+  );
+
   return (
-    <div>
-      <TransactionTable />
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="mt-8 flow-root">
+        <div className="flex flex-row justify-between items-center">
+          <div>
+            {livePayments ? (
+              <span>
+                Data is being fetched{" "}
+                <span className="text-red-500 font-bold">LIVE</span>
+              </span>
+            ) : (
+              <span>
+                Data fetching is on{" "}
+                <span className="text-blue-500 font-bold">PAUSE</span>
+              </span>
+            )}
+          </div>
+          <Button
+            className={classnames(
+              "rounded py-2 px-4 text-sm text-white w-24 font-bold",
+              livePayments
+                ? "bg-red-600 data-[hover]:bg-red-500 data-[active]:bg-red-700"
+                : "bg-sky-600 data-[hover]:bg-sky-500 data-[active]:bg-sky-700"
+            )}
+            onClick={toggleLivePayments}
+          >
+            {livePayments ? "LIVE" : "PAUSED"}
+          </Button>
+        </div>
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <TransactionTable />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/organisms/TransactionTable/TransactionTable.tsx
+++ src/components/organisms/TransactionTable/TransactionTable.tsx
@@ -10,33 +10,25 @@ const TransactionTable = () => {
   const { getPayments } = usePaymentsStore((state) => state);
 
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
-      <div className="mt-8 flow-root">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <Table>
-              <thead>
-                <tr>
-                  {TableHead.map((columnName, index) => (
-                    <Table.Head text={columnName} key={columnName + index} />
-                  ))}
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200 bg-white">
-                {getPayments(25).map((transaction) => (
-                  <tr key={transaction.id} className="hover:bg-slate-100">
-                    <Table.Data data={transaction?.sender?.name} />
-                    <Table.Data data={transaction?.receiver?.name} />
-                    <Table.Data data={transaction?.amount} />
-                    <Table.Data data={transaction?.currency} />
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Table>
+      <thead>
+        <tr>
+          {TableHead.map((columnName, index) => (
+            <Table.Head text={columnName} key={columnName + index} />
+          ))}
+        </tr>
+      </thead>
+      <tbody className="divide-y divide-gray-200 bg-white">
+        {getPayments(25).map((transaction) => (
+          <tr key={transaction.id} className="hover:bg-slate-100">
+            <Table.Data data={transaction?.sender?.name} />
+            <Table.Data data={transaction?.receiver?.name} />
+            <Table.Data data={transaction?.amount} />
+            <Table.Data data={transaction?.currency} />
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 };
 
