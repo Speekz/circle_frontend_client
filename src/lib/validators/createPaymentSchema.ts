81114647d5d2fecd50c504961fdb9d3936b4307f
Johan Bejar <johanbejarc@hotmail.com>
2024-10-03 18:09:03 -0500

    fix(create-payment-schema): added refinement to check if sender and receiver is the same

--- src/lib/validators/createPaymentSchema.ts
+++ src/lib/validators/createPaymentSchema.ts
@@ -1,18 +1,22 @@
 import { z } from "zod";
 
-export const createPaymentSchema = z.object({
-  transactionId: z.string().length(16),
-  sender: z.string(),
-  receiver: z.string(),
-  amount: z.string().refine(
-    (v) => {
-      const n = Number(v);
-      return !isNaN(n) && v?.length > 0 && n !== 0;
-    },
-    { message: "Invalid number" }
-  ),
-  currency: z.string(),
-  memo: z.string().optional(),
-});
+export const createPaymentSchema = z
+  .object({
+    transactionId: z.string().length(16),
+    sender: z.string(),
+    receiver: z.string(),
+    amount: z.string().refine(
+      (v) => {
+        const n = Number(v);
+        return !isNaN(n) && v?.length > 0 && n !== 0;
+      },
+      { message: "Invalid number" }
+    ),
+    currency: z.string(),
+    memo: z.string().optional(),
+  })
+  .refine((data) => data.sender !== data.receiver, {
+    message: "Sender and receiver should be different",
+  });
 
 export type TPaymentSchema = z.infer<typeof createPaymentSchema>;
