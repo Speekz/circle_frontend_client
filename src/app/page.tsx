3ab0dabe96e52fa0f572b93d3059487e63d45e7b
Johan Bejar <johanbejarc@hotmail.com>
2024-10-03 02:16:33 -0500

    feat(payments): get payments every second and store it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,67 @@
+"use client";
+import { getPayments } from "@/hooks/usePayments";
+import { usePaymentsStore } from "@/store/usePaymentsStore";
+
 export default function Home() {
-  return <div>Hi</div>;
+  getPayments();
+  const { payments } = usePaymentsStore((state) => state);
+
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Sender Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Receiver Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Amount
+                  </th>
+                  <th
+                    scope="col"
+                    className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Currency
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {payments.map((transaction) => (
+                  <tr key={transaction.id} className="hover:bg-slate-100">
+                    <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+                      {transaction?.sender?.name}
+                    </td>
+                    <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
+                      {transaction?.receiver?.name}
+                    </td>
+                    <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                      {transaction?.amount}
+                    </td>
+                    <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                      {transaction?.currency}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
